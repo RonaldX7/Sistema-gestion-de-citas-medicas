65d1c35c89b69d4676ddac2c6892b256f4f4cdc1
RonaldX7 <ronaldjair0705@gmail.com>
2024-10-07 16:17:18 -0500

    mapeado la entidad costo de citas

--- backend/src/main/java/com/project/integradorII/entities/AppointmentCosts.java
+++ backend/src/main/java/com/project/integradorII/entities/AppointmentCosts.java
@@ -1,13 +1,16 @@
 package com.project.integradorII.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class AppointmentCosts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private double cost;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "specialty_id")
+    private SpecialtyEntity specialty;
 }
