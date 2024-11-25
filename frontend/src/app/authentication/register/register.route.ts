c1043f171cb50e27bad2305e5becfef0bc43345c
josueprogrammerzz <josuesuasnabar@gmail.com>
2024-11-24 23:05:24 -0500

    install sweetaler

--- frontend/src/app/authentication/register/register.route.ts
+++ frontend/src/app/authentication/register/register.route.ts
@@ -1,17 +1,9 @@
 import { Routes } from "@angular/router";
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/josue
 export default[
     {
         path:'register',
         loadComponent:() => import('./register.component').then(m=>m.RegisterComponent)
     }
-<<<<<<< HEAD
 
 ] as Routes;
-=======
-] as Routes;
->>>>>>> origin/josue
